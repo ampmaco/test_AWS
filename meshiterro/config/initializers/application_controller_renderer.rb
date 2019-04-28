2a83cc9b9ef1499fb594e06b651bf04800fa9be7
ampmaco <ampmaco@gmail.com>
2019-04-28 15:46:46 +0900

    AWS UPLOAD TEST

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2fd80efee00a001f1b2216aa187781cc04ebba91046f55c9eb4229057659e45e2132710faf3f8ff271892ee7c7956416ff4c18adf281621baa23fe92dd27b4e5'
